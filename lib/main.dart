e84bcdf52712608898cd51c2ab8208404c8d5f33
ikays97 <ikays.holding@gmail.com>
2021-03-04 04:04:00 -0600

    starter touches

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,18 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(IKINOKATAPP());
+  runApp(
+    EasyLocalization(
+      path: 'assets/locales',
+      supportedLocales: [
+        Locale('en', 'UK'),
+        Locale('ru', 'RU'),
+        Locale('tk', 'TM')
+      ],
+      child: IKINOKATAPP(),
+    ),
+  );
 }
 
 class IKINOKATAPP extends StatelessWidget {
